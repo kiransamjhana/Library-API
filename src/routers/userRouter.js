48da124352313e1fbe1bae8e8339b56aca74f9b3
kiransamjhana <kiranmanandhar956@gmail.com>
2023-07-01 13:13:42 +1000

    new router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { insertUser } from "../models/user/UserModel.js";
-import { hashPassword } from "../utils/bcrypt.js";
+import { getUserByEmail, insertUser } from "../models/user/UserModel.js";
+import { comparePassword, hashPassword } from "../utils/bcrypt.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -41,6 +41,38 @@ router.post("/", async (req, res) => {
     res.json({
       statu: "error",
       message: msg,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    //get the data
+    const { email, password } = req.body;
+    // check if user exit with received email and get user form db
+    const user = await getUserByEmail(email);
+    //use bcyrpt to check if password is matching
+
+    if (user?._id) {
+      const isMatch = comparePassword(password, user.password);
+      if (isMatch) {
+        user.password = undefined;
+        return res.json({
+          status: "success",
+          message: "Logedin successfully",
+          user,
+        });
+      }
+    }
+
+    res.json({
+      status: "error",
+      message: "Please check your password",
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
     });
   }
 });
--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -4,3 +4,7 @@ const salt = 10;
 export const hashPassword = (plainPassword) => {
   return bcrypt.hashSync(plainPassword, salt);
 };
+
+export const comparePassword = (plainPassword, hashPass) => {
+  return bcrypt.compareSync(plainPassword, hashPass);
+};
